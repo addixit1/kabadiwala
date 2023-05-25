dd688c2124306cf1f651337332848445fba30000
Aman Dixit <88907029+addixit1@users.noreply.github.com>
2023-05-25 14:45:47 +0530

    Update Cart.js

--- src/PAGES/Cart/Cart.js
+++ src/PAGES/Cart/Cart.js
@@ -88,7 +88,7 @@ const Cart = () => {
       }
       <SingleBanner
         heading="My Cart"
-        bannerimage='https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+        bannerimage='https://images.unsplash.com/photo-1448697138198-9aa6d0d84bf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
       />
       <div className='cart'>
         <div className='progress'>
